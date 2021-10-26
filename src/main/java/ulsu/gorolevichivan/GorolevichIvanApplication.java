9f012600b70cab3141112d2c6256695804ed640c
Иван Горолевич <gorolevichip@gmail.com>
2021-10-26 14:39:03 +0400

    init

---
+++ src/main/java/ulsu/gorolevichivan/GorolevichIvanApplication.java
@@ -0,0 +1,13 @@
+package ulsu.gorolevichivan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GorolevichIvanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GorolevichIvanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ulsu/gorolevichivan/GorolevichIvanApplicationTests.java
@@ -0,0 +1,13 @@
+package ulsu.gorolevichivan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GorolevichIvanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
