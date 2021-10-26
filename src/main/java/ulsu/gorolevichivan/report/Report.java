b60bd01dc01ef89ef499bc0c118a653f79f20633
Иван Горолевич <gorolevichip@gmail.com>
2021-10-26 14:43:14 +0400

    report init

---
+++ src/main/java/ulsu/gorolevichivan/report/Report.java
@@ -0,0 +1,8 @@
+package ulsu.gorolevichivan.report;
+
+public interface Report {
+
+    String getMessage();
+
+    String setMessage();
+}
---
+++ src/main/java/ulsu/gorolevichivan/report/TaskReport.java
@@ -0,0 +1,14 @@
+package ulsu.gorolevichivan.report;
+
+public class TaskReport implements Report {
+
+    @Override
+    public String getMessage() {
+        return null;
+    }
+
+    @Override
+    public String setMessage() {
+        return null;
+    }
+}
